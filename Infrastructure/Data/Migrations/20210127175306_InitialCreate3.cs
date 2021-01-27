edf6d198de471c03f4112305ddaea96cced49343
mk5cs <andresvasile.av@gmail.com>
2021-01-27 15:25:31 -0300

    End of section 3

---
+++ Infrastructure/Data/Migrations/20210127175306_InitialCreate3.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class InitialCreate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "TEXT",
+                maxLength: 180,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 180);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "TEXT",
+                maxLength: 180,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 180,
+                oldNullable: true);
+        }
+    }
+}
