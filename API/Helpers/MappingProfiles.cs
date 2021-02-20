08656c327e847b7615c720e06c776026a5bc40a2
mk5cs <andresvasile.av@gmail.com>
2021-02-20 15:03:40 -0300

    End of section 16

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -24,6 +24,8 @@ namespace API.Helpers
                     o =>
                         o.MapFrom<ProductUrlResolver>());
             CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<CustomerBasketDto, CustomerBasket>();
+            CreateMap<BasketItemDto, BasketItem>();
         }
     }
 }
